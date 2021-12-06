4b86d4f1c42cc35c8e0009b60e51b160d516006a
Hamish <hamish.gamble@flinders.edu.au>
2021-12-06 13:31:04 +1030

    Helper function for tablebuilder spreadsheets

---
+++ R/tb_helper.R
@@ -0,0 +1,29 @@
+#' Help for reading table builder files with 10 Year Groups and Sex
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tb_helper <- function() {
+  age <- c("15-19 years",
+           "20-29 years",
+           "30-39 years",
+           "40-49 years",
+           "50-59 years",
+           "60-69 years",
+           "70-79 years",
+           "80-89 years",
+           "90-99 years",
+           "100 years and over",
+           "Total")
+  
+  sex <- c("Male", "Female")
+  
+  l <- data.frame(
+    sheet = mtwp_sheets,
+    age = age, 
+    sex = sex
+  )
+  
+  return(l)
+}
